d584eadb32ee635bc0d45e9fdb617a6e3daacf13
David Elutilo <delutilo.dev@gmail.com>
2018-12-06 20:11:52 -0600

    expose build

---
+++ app/build/precache-manifest.fc60cdaa8c30963153aba0ad4ff1dace.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d5b9c78e907c271e159db2a77f5439f",
+    "url": "/static/media/robot.0d5b9c78.svg"
+  },
+  {
+    "revision": "6988cfc5150ff6e9c991fc38679ad0c3",
+    "url": "/static/media/Silkscreen.6988cfc5.ttf"
+  },
+  {
+    "revision": "51dc872a80d3f1971f678d3830af259d",
+    "url": "/static/media/Silkscreen.51dc872a.woff"
+  },
+  {
+    "revision": "0b0c52ed91ae27bc5b6a03526b3b06bc",
+    "url": "/static/media/Silkscreen.0b0c52ed.woff2"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c806bbd8cac2df71212c",
+    "url": "/static/js/main.c806bbd8.chunk.js"
+  },
+  {
+    "revision": "2ae536b58c1579e7e279",
+    "url": "/static/js/1.2ae536b5.chunk.js"
+  },
+  {
+    "revision": "c806bbd8cac2df71212c",
+    "url": "/static/css/main.36cb369b.chunk.css"
+  },
+  {
+    "revision": "5d84739b4c2a6e0b862ee5660f62bc89",
+    "url": "/index.html"
+  }
+];
